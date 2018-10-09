443630cc6b181bc591843cbb11143f72d71c7e12
muggy8 <muggy8@gmail.com>
2018-10-09 13:34:10 -0700

    whoo everything works

--- form.js
+++ form.js
@@ -5,16 +5,18 @@ App.Components.Form = (function({form, div, input, label, span}){
             this.state = props.state || {}
             this.state.name
             this.state.age
+
+            console.log(this)
         }
         render(){
             return form({onSubmit: ()=>false},
                 label({},
                     span({}, "Name"),
-                    input({type: "text", name: "name", onChange: this.update("this.state.name"), value: this.state.name}),
+                    input({type: "text", name: "name", onChange: this.update("name"), value: this.state.name}),
                 ),
                 label({},
                     span({}, "Age"),
-                    input({type: "number", name: "age", onChange: this.update("this.state.age"), value: this.state.name}),
+                    input({type: "number", name: "age", onChange: this.update("age"), value: this.state.age}),
                 ),
                 input({type: "submit", value: "submit"}),
                 div({},
