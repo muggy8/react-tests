85cd35095007555de47b1b754f4a0236cb74ece1
muggy8 <muggy8@gmail.com>
2018-10-09 12:24:08 -0700

    ok maybe i need to use the set state function instead

--- form-component.js
+++ form-component.js
@@ -1,23 +1,16 @@
-App.Components.FormComponent = (function(safeEval){
+App.Components.FormComponent = (function(){
     return class FormComponent extends React.Component {
-        constructor(){
-            super()
-            this.updateCache = {} // this is public to give future ability to clear the cashe if needed
+        constructor(props, refs, updater){
+            super(props, refs, updater)
+            this.updateCache = {} // this is public to give future ability to clear the cache if needed
         }
 
         update(location){
             var context = this
             return this.updateCache[location] || (this.updateCache[location] = function(ev){
-                let value = ev.target.value
-                safeEval.call(context, "this.state." + location + "=value", {value})
+                // let value = ev.target.value
+                context.setState({[location]: ev.target.value})
             })
         }
     }
-})(function(s, v = null){
-    if (v){
-        with(v){
-            return eval(s)
-        }
-    }
-    return eval(s)
-})
+})()
