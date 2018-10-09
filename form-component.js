9b8f4801591fc3c1735cd94f15771854918f9ba4
muggy8 <muggy8@gmail.com>
2018-10-09 10:47:12 -0700

    cache functions

--- form-component.js
+++ form-component.js
@@ -1,11 +1,16 @@
 App.Components.FormComponent = (function(safeEval){
     return class FormComponent extends React.Component {
+        constructor(){
+            super()
+            this.updateCache = {} // this is public to give future ability to clear the cashe if needed
+        }
+
         update(location){
             var context = this
-            return function(ev){
+            return this.updateCache[location] || (this.updateCache[location] = function(ev){
                 let value = ev.target.value
                 safeEval.call(context, "this.state." + location + "=value", {value})
-            }
+            })
         }
     }
 })(function(s, v = null){
